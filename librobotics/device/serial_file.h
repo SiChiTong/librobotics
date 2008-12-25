93a4c91f27836b68db81c0403f09fca19d746f76
mahisorn.w <mahisorn.w@1f4a4ea8-a174-11dd-adfd-a1ec4d499d49>
2008-12-25 08:14:59 +0000

    Add  - Hokuyo URG SCIP1.0/SCIP2.0 interface  - Serial port connection as file in Linux

---
+++ librobotics/device/serial_file.h
@@ -0,0 +1,153 @@
+#ifndef _serial_file_h_
+#define _serial_file_h_
+
+#include <librobotics.h>
+
+#if (librobotics_OS == 1)
+#include <sys/termios.h>
+#else
+#error Not yet support OS
+#endif
+
+/**
+ *  Base class for serial port communicate device
+ */
+class serial_file {
+public:
+    serial_file() {
+        file = 0;
+    }
+    virtual ~serial_file() { }
+
+    /** Open and set up specified serial port
+     * @param aDevNme [in] Serial port device file name
+     * @param aSpeed [in] Communication speed(bps)
+     * @return Serial port file handle
+     * Returns NULL when failed to open.
+     */
+    int open(const char *aDevName, int aSpeed) {
+        file = fopen(aDevName, "w+");
+
+        if (file == NULL) {
+           return -1;  // invalid device file
+        }
+
+        // setup parameters
+        if (setup_serial(fileno(file), aSpeed))
+           return 0;
+
+        // fail
+        perror("Cannot open serial device");
+        fclose(file);
+
+        file = 0;
+        return -1;
+    }
+
+    bool is_open() {
+        return (file != 0);
+    }
+
+    int close() {
+        if (file) {
+           if( fclose(file) != 0)
+               return -1;
+           file = 0;
+        }
+        return 0;
+    }
+
+protected:
+
+    /** Parameter settings for serial port communication
+     * @param aFd [in] File descriptor to point serial port
+     * @param aSpeed [in] Communication speed (bps)
+     * @retval 0 Failed
+     * @retval 1 Success
+     */
+    int setup_serial(int aFd, int aSpeed) {
+        int speed;
+
+        struct termios tio;
+
+        speed = bitrate(aSpeed);
+
+        if (speed == 0)
+            return 0;  // invalid bitrate
+
+        tcgetattr(aFd, &tio);
+
+        cfsetispeed(&tio, speed); // bitrate
+
+        cfsetospeed(&tio, speed); // bitrate
+
+        tio.c_cflag = (tio.c_cflag & ~CSIZE) | CS8; // data bits = 8bit
+
+        tio.c_iflag &= ~(BRKINT | ICRNL | ISTRIP);
+
+        tio.c_iflag &= ~ IXON;   // no XON/XOFF
+
+        tio.c_cflag &= ~PARENB;  // no parity
+
+        tio.c_cflag &= ~CRTSCTS; // no CTS/RTS
+
+        tio.c_cflag &= ~CSTOPB;  // stop bit = 1bit
+
+        // Other
+        tio.c_lflag &= ~(ISIG | ICANON | ECHO);
+
+        // Commit
+        if (tcsetattr(aFd, TCSADRAIN, &tio) == 0)
+            return 1;
+
+        return 0;
+    }
+
+
+    /** SPEED SETTINGS
+     * @param aBR [in] Declared for communication speed (4800 -500000)
+     * @return cfset[i|o] Returned value for speed function
+     * Returns B0 if failed
+     */
+    speed_t bitrate(int aBR) {
+        switch (aBR)
+        {
+           case 4800:
+                 return B4800;
+
+           case 9600:
+               return B9600;
+
+           case 19200:
+               return B19200;
+
+           case 38400:
+               return B38400;
+
+           case 57600:
+               return B57600;
+
+           case 115200:
+               return B115200;
+
+           case 230400:
+               return B230400;
+
+           case 460800:
+               return B460800;
+
+           case 500000:
+               return B500000;
+
+           default: // invalid bitrate
+               return B0;
+        }
+    }
+
+    FILE* file;
+};
+
+
+
+
+#endif
---
+++ librobotics/test/test_device.cpp
@@ -0,0 +1,81 @@
+/*
+ * test_device.cpp
+ *
+ *  Created on: Dec 25, 2008
+ *      Author: mahisorn
+ */
+
+#if 1
+
+
+#include "../librobotics.h"
+#include <boost/thread/thread.hpp>
+#include <iostream>
+#include "device/hokuyo_urg.h"
+
+using namespace std;
+
+#define TIME 10000
+
+hokuyo_urg urg[3];
+
+void tr(hokuyo_urg& urg, int id) {
+    vector<unsigned int> ranges;
+    unsigned int time;
+    for(int i = 0; i < TIME; i++) {
+        urg.get_data_2char_scip20(0, 768, 1, ranges, time);
+        cout << "urg[" << id << "] update: " << time << "\n";
+    }
+}
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+    PRINTVAR(urg[0].open("/dev/ttyACM0", 115200));
+    PRINTVAR(urg[1].open("/dev/ttyACM1", 115200));
+    PRINTVAR(urg[2].open("/dev/ttyACM2", 115200));
+
+//    urg[0].enable_scip20();
+//    urg[1].enable_scip20();
+//    urg[2].enable_scip20();
+
+    urg[0].laser_on_scip20();
+    urg[1].laser_on_scip20();
+    urg[2].laser_on_scip20();
+
+    boost::thread thrd0(&tr, urg[0], 0);
+    boost::thread thrd1(&tr, urg[1], 1);
+    boost::thread thrd2(&tr, urg[2], 2);
+
+    thrd0.join();
+    thrd1.join();
+    thrd2.join();
+
+//    unsigned long start = librobotics::utils_get_current_time();
+
+//    for(int i = 0; i < 10000; i++) {
+//
+//        PRINTVAR(time);
+//        urg[1].get_data_2char_scip20(0, 768, 1, ranges, time);
+//        PRINTVAR(time);
+//        urg[2].get_data_2char_scip20(0, 768, 1, ranges, time);
+//        PRINTVAR(time);
+//        PRINTVAR((librobotics::utils_get_current_time() - start));
+//    }
+
+    urg[0].laser_off_scip20();
+    urg[1].laser_off_scip20();
+    urg[2].laser_off_scip20();
+
+    urg[0].close();
+    urg[1].close();
+    urg[2].close();
+
+
+
+    return 0;
+}
+
+
+#endif
+
